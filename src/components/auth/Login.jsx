67c50a43d616d7584dec4a79862cb63b68ee828f
Suvro Biswas <shuvo.249963@gmail.com>
2026-02-26 00:35:30 +0600

    move login to auth folder and update routes

---
+++ src/components/auth/Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+    <div className='text-green-500 font-extrabold text-5xl h-screen flex justify-center items-center '>
+        This is login page
+    </div>
+  )
+}
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,14 +1,11 @@
 import { createBrowserRouter } from "react-router";
+import Login from "../components/auth/Login";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    element: <Login/>,
     errorElement: <div>Hello World</div>,
-    children:[
-        {
-            
-        }
-    ]
+   
   },
 ]);
