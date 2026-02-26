96f470564f42bd1c4d74c4b30cf4471a3b940ff8
Suvro Biswas <shuvo.249963@gmail.com>
2026-02-26 22:43:05 +0600

    Update error page component

--- src/components/errorPage/ErrorPage.jsx
+++ src/components/errorPage/ErrorPage.jsx
@@ -5,15 +5,15 @@ import errorImg from '../../assets/errorImg.webp'
 export default function ErrorPage() {
     const error = useRouteError();
     return (
-        <div className=' h-screen flex flex-col justify-center items-center space-y-3  px-4'>
-            <img src={errorImg} alt='Error Image' className='rounded-xl' />
+        <div className='  flex flex-col justify-center items-center space-y-3 py-4  px-4'>
+            <img src={errorImg} alt='Error Image' className='rounded-xl h-105 w-112.5' />
             <h1 className='text-3xl font-bold'>Oops, page not found!!</h1>
             <p className="text-muted ">
                 {error?.statusText || error?.message || "The page you are looking for is not available."}
             </p>
             <Link to='/'
 
-                className="btn bg-green-500 hover:bg-green-700 text-white border-none"
+                className="btn bg-green-800 hover:bg-green-900 text-white border-none px-4 py-2 rounded-xl transition"
             >
                 Go Back !
 
